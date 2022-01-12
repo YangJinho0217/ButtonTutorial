cd892d11bdb18826901da006737fefd1e3be1ee3
YagSill <73631818+YangJinho0217@users.noreply.github.com>
2022-01-12 14:09:12 +0900

    버튼을 활용한 Label 변경

--- ButtonTutorial/ViewController.swift
+++ ButtonTutorial/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var FirstBtn: UIButton!
+    @IBOutlet var SecondBtn: UIButton!
+    @IBOutlet var Text: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func ActionBtn(_ sender: Any) {
+        print("왼쪽버튼눌림")
+        Text.text = "왼쪽"
+    }
+    @IBAction func ActionSBtn(_ sender: Any) {
+        print("오른쪽버튼눌림")
+        Text.text = "오른쪽"
+    }
+    
 }
 
